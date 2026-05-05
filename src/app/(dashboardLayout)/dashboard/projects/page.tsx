1603b515f1baef782da8f10153b6a26cda0264b1
MehediMehad <mdmehedihasanmehad@gmail.com>
2026-05-05 22:55:36 +0600

    feat: replace ProjectCard with AdminProjectCard for admin users; implement admin-specific actions

--- src/app/(dashboardLayout)/dashboard/projects/page.tsx
+++ src/app/(dashboardLayout)/dashboard/projects/page.tsx
@@ -1,4 +1,4 @@
-import { ProjectCard } from "@/components/shared/Card/ProjectCard";
+import { AdminProjectCard } from "@/components/shared/Card/AdminProjectCard";
 import TablePagination from "@/components/shared/TablePagination";
 import { TableSkeleton } from "@/components/shared/TableSkeleton";
 import { queryStringFormatter } from "@/lib/formatters";
@@ -31,7 +31,7 @@ const DashboardProjectsPage = async ({
         {projects.length > 0 ? (
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
             {projects.map((project) => (
-              <ProjectCard key={project.slug} project={project} isAdmin />
+              <AdminProjectCard key={project.slug} project={project} isAdmin />
             ))}
           </div>
         ) : (
