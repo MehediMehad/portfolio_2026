673b9c5e0eaf7bb67973d94cb2d1e097835ac730
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-05-07 11:55:15 +0600

    add base api

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server"
 
+import { BACKEND_API_URL } from "@/lib/server-fetch";
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
-
+const baseAPI = BACKEND_API_URL;
 
 export const loginUser = async (userData: FieldValues) => {
     try {
         // https://portfolio-server-neon-alpha.vercel.app 
-        const res = await fetch(`http://localhost:5000/api/v1/auth/login`, {
+        const res = await fetch(`${baseAPI}/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
