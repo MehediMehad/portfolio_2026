012b3caad14a35df706ebe596ff03ab462081be5
MehediMehad <mdmehedihasanmehad@gmail.com>
2026-05-11 17:34:31 +0600

    build error fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import LoginForm from "@/components/modules/Login/LoginForm";
 import { Suspense } from "react";
 
--- src/app/(commonLayout)/blogs/page.tsx
+++ src/app/(commonLayout)/blogs/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import BlogSearchFilters from "@/components/modules/Blogs/BlogSearchFilters";
 import { BlogCard } from "@/components/shared/Card/BlogCard";
 import TablePagination from "@/components/shared/TablePagination";
